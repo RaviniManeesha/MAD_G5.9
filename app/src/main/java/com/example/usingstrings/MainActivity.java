b298a1f2ea0b7ad09ddfb178f6a0b040e51635e6
Diwyangi Vithana <61859959+diwyangi98@users.noreply.github.com>
2020-08-08 19:08:36 +0530

    Add Log class

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtView2;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
         txtView2 = findViewById(R.id.textView2);
         txtView2.setText(R.string.Msg2);
+        Log.i(TAG, "onCreate: ");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+
+
 }
