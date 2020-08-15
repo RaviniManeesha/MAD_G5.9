8c84fa530a0034b72fb7887460e655b9abc9d66a
Diwyangi Vithana <61859959+diwyangi98@users.noreply.github.com>
2020-08-15 17:44:04 +0530

    Add Changes

--- app/src/main/java/com/example/mad_059/addsubmissison.java
+++ app/src/main/java/com/example/mad_059/addsubmissison.java
@@ -3,6 +3,8 @@ package com.example.mad_059;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class addsubmissison extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class addsubmissison extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addsubmissison);
+
     }
 }
