1391da28ec1243bd2c416cd203d7d32ec94dc93e
Diwyangi Vithana <61859959+diwyangi98@users.noreply.github.com>
2020-08-16 20:59:51 +0530

    Add MAD Project

--- app/src/main/java/com/example/mad_059/resetpwd2.java
+++ app/src/main/java/com/example/mad_059/resetpwd2.java
@@ -2,13 +2,31 @@ package com.example.mad_059;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class resetpwd2 extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpwd2);
+
+        button =(Button) findViewById(R.id.button7);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
+    }
+
+
+    public void openLogin(){
+
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
     }
 }
