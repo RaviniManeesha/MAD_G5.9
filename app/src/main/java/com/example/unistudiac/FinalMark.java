d7a66ac1f23e01a6e2238e3c39908f95081921ba
Ravini Maneesha <it19064246@my.sliit.lk>
2020-10-04 21:56:49 +0530

    Final Commit

--- app/src/main/java/com/example/unistudiac/FinalMark.java
+++ app/src/main/java/com/example/unistudiac/FinalMark.java
@@ -2,7 +2,6 @@ package com.example.unistudiac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,52 +35,46 @@ public class FinalMark extends AppCompatActivity {
         });
 
     }
-    public void marksCal(){
-        int cam, fnm, tot;
-        String grd;
+    public int marksCal(){
+        int tot;
 
-        cam = Integer.parseInt(ca.getText().toString());
-        fnm = Integer.parseInt(fn.getText().toString());
-        //tot = Integer.parseInt(total.getText().toString());
+        int cam = Integer.parseInt(ca.getText().toString());
+        int fnm = Integer.parseInt(fn.getText().toString());
 
-        tot = (cam + fnm) / 2;
+        tot = calculate(cam, fnm);
 
         total.setText(String.valueOf(tot));
-
 
         if(tot >= 90){
             grade.setText("A+");
         }
         else if(tot >= 80 && tot <= 89){
             grade.setText("A");
-
         }
         else if(tot >= 75 && tot <= 79){
             grade.setText("A-");
-
         }
         else if(tot >= 70 && tot <= 74){
             grade.setText("B+");
-
         }
         else if(tot >= 65 && tot <= 69){
             grade.setText("B");
-
         }
         else if(tot >= 60 && tot <= 64){
             grade.setText("B-");
-
         }
         else if(tot >= 55 && tot <= 59){
             grade.setText("C+");
-
         }
         else if(tot >= 45 && tot <= 54){
             grade.setText("C");
-
         }
         else{
             grade.setText("F");
         }
+        return cam;
+    }
+    public int calculate(int cam, int fnm){
+        return (cam + fnm) / 2;
     }
 }
--- app/src/test/java/com/example/unistudiac/ExampleUnitTest.java
+++ app/src/test/java/com/example/unistudiac/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.unistudiac;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+    private FinalMark finalMark;
+
+    @Before
+    public void setup(){
+        finalMark = new FinalMark();
     }
+
+    @Test
+    public void testMarksCal(){
+        int result;
+        result = finalMark.calculate(90,80);
+        assertEquals(85, result);
+    }
+
+
+
 }
